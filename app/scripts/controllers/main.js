0fda6fa814e513d0a42b9eb7c8a0f1e6b31f2e84
Scott Nath <github@scottnath.com>
2014-06-08 21:54:23 -0400

    delete working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,12 +5,34 @@ define(['angular'], function (angular) {
     .controller('MainCtrl', function ($scope,$location,Restangular) {
       $scope.tobuys = Restangular.all('to-buy-items').getList().$object;
 
-      $scope.destroy = function(tobuy) {
-        console.log(tobuy);
-        tobuy.remove().then(function() {
-          $location.path('/list');
-        });
+      $scope.keyPress = function ($event) {
+        console.log($event);
+      };
+      $scope.$watch('keyCode', function() {
+        console.log("controller: " + $scope.keyCode);
+        //$location.path('#/add');
+      });
+
+      $scope.destroy = function(tobuys,index) {
+        console.log(tobuys[index]._id.$oid);
+        var tobuyId = tobuys[index]._id.$oid;
+        tobuys.splice(index, 1);
+        Restangular.one('to-buy-items',tobuyId).remove();
       };
 
-    });
+    })
+    .directive('keyCapture', [function() {
+      return {
+        link: function (scope, element, attrs, controller,location) {
+            console.log("ELM: "+element);
+              element.on('keydown', function(e){
+                  console.log("CODE: "+e.keyCode);
+                  console.log(location)
+                  scope.keyCode = "YES";
+              });
+
+          }
+        }
+      }]
+    );
 });
